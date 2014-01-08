ccf8012d938e9c0221e3baeae5f1deef9d31e95d
Isaac Zepeda <keogh24@gmail.com>
2014-01-07 18:07:31 -0700

    Sign in with Instagram button. Get and store access_token

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,7 +12,12 @@ angular.module('insta5App', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/access_token=:access_token', {
+        templateUrl: 'views/main.html',
+        controller: 'AccessCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  }]);
+  }])
+  .constant('CLIENT_ID', '5dbbf3b5d3614611a0a405289ab9ce42');
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('insta5App')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', '$cookies', 'CLIENT_ID', function ($scope, $cookies, CLIENT_ID) {
+    window.cookies = $cookies;
+
+    var accessToken = $cookies.accessToken,
+      redirectUri = encodeURI('http://127.0.0.1:9000');
+    
+    $scope.instagramAuthUrl = 'http://instagram.com/oauth/authorize/?client_id='+CLIENT_ID+'&redirect_uri='+redirectUri+'&response_type=token';
+
   }]);
